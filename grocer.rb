1789437b86229a544329cc771d06069770f838b7
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-30 15:41:22 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,13 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+
+  collection.find do |element|
+    element[:item] == name
+  end
 end
 
 def consolidate_cart(cart)
@@ -9,18 +15,62 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  newcart = []
+  cart.each do |food|
+    record = newcart.find do |record|
+      record[:item] == food[:item]
+    end
+    if record
+      record[:count] += 1
+    else
+      newfood = food
+      newfood[:count] = 1
+      newcart << newfood
+    end
+  end
+  newcart
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < coupons.length
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupon[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
+      else
+        cart_item_with_coupon = {
+          :item => couponed_item_name,
+          :price => coupons[counter][:cost]/coupons[counter][:num],
+          :count => coupons[counter][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
+      end
+    end
+    counter += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
+
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart.map do |i|
+    if i[:clearance] == true
+      i[:price] = (i[:price] * 0.8).round(2)
+    end
+    i
+  end
 end
 
 def checkout(cart, coupons)
@@ -33,4 +83,17 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  cleared_cart = apply_clearance(couponed_cart)
+
+  totalcost = cleared_cart.reduce(0) do |total, food|
+    total + food[:price] * food[:count]
+  end
+
+  if totalcost > 100
+    totalcost = (totalcost * 0.9).round(2)
+  end
+  totalcost
 end
